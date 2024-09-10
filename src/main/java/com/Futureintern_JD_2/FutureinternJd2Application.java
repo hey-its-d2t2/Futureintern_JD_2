c5b11e31ba797b4f9934a4e8726a2ae1eefbadb4
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-10 19:13:51 +0530

    Initial commit, project created from start.spring.io with these configuration java 17, maven ,jar, spring 3.3.3, spring web, devtools, thymeleaf.

---
+++ src/main/java/com/Futureintern_JD_2/FutureinternJd2Application.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureinternJd2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureinternJd2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Futureintern_JD_2/FutureinternJd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Futureintern_JD_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureinternJd2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
