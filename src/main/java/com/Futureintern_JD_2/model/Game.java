07c20453c4a9e171f323bf6577d63dd92540a31a
Deepak Singh <deepsinghkumar01@gmail.com>
2024-09-11 13:05:16 +0530

    Game Model implemented, contains all game logic.

--- src/main/java/com/Futureintern_JD_2/model/Game.java
+++ src/main/java/com/Futureintern_JD_2/model/Game.java
@@ -1,4 +1,49 @@
 package com.Futureintern_JD_2.model;
 
+import java.util.Random;
+
 public class Game {
+    private String userChoice;
+    private String computerChoice;
+    private String result;
+
+    public void setUserChoice(String userChoice) {
+        this.userChoice = userChoice;
+        this.computerChoice = generateComputerChoice();
+        this.result = determineWinner();
+    }
+
+    private String generateComputerChoice() {
+        String[] choices = {"Rock", "Paper", "Scissors"};
+        Random random = new Random();
+        return choices[random.nextInt(choices.length)];
+    }
+
+    private String determineWinner() {
+        if (userChoice.equals(computerChoice)) {
+            return "Draw!";
+        }
+        switch (userChoice) {
+            case "Rock":
+                return (computerChoice.equals("Scissors")) ? "You Win!" : "Computer Wins!";
+            case "Paper":
+                return (computerChoice.equals("Rock")) ? "You Win!" : "Computer Wins!";
+            case "Scissors":
+                return (computerChoice.equals("Paper")) ? "You Win!" : "Computer Wins!";
+            default:
+                return "Invalid choice!";
+        }
+    }
+
+    public String getUserChoice() {
+        return userChoice;
+    }
+
+    public String getComputerChoice() {
+        return computerChoice;
+    }
+
+    public String getResult() {
+        return result;
+    }
 }
